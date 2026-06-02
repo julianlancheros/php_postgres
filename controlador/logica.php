afb4551ab93798890901f4be554e957db6493d2a
julianlancheros <julian.lancheros.131@gmail.com>
2026-06-01 21:30:48 -0500

    Update database connection and table names in logic.php

--- controlador/logica.php
+++ controlador/logica.php
@@ -2,12 +2,12 @@
 
 var_dump($_POST);
 
-$conexion = new PDO('pgsql:host=servidor;dbname=nombre_BD','user','password');
-$registrar = $conexion->prepare("INSERT INTO form_sugerencias (nombre,telefono,detalles) VALUES (?, ?, ?)");
+$conexion = new PDO('pgsql:host=dpg-d8f39f553gjs739kr4mg-a.oregon-postgres.render.com;dbname=practica_sovw','practica_sovw_user','cdfeR6wk4p1M7xvDQhLPIXPx2OeOYZWq');
+$registrar = $conexion->prepare("INSERT INTO aprendizes (nombre,telefono,detalles) VALUES (?, ?, ?)");
 $registrar->execute([$_POST["nom"], $_POST["tel"], $_POST["det"]]);
 echo "<p style='color:white;background-color:green;font-family:calibri,arial;font-size:24px;text-align:center'>Registro exitoso</p>";
 
-$consulta = $conexion->prepare("SELECT * FROM form_sugerencias order by id");
+$consulta = $conexion->prepare("SELECT * FROM aprendizes order by id");
 $consulta->execute();
 $tabla = $consulta->fetchAll(PDO::FETCH_ASSOC);	      //PDO::FETCH_NUM
 $conexion = null;
